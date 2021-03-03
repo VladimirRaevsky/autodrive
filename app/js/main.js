5a6e361f66bcaabd800c66f0f241b4a3f76f2eeb
vladimir <rayevskiy.volodya@mail.ru>
2021-03-03 20:05:10 +0300

    finished the first page layout

---
+++ app/js/main.js
@@ -0,0 +1,11 @@
+window.addEventListener('DOMContentLoaded', ()=> {
+'use strict';
+
+
+    const firstElement = document.querySelectorAll('.header__item')[0];
+    const lastElement = document.querySelectorAll('.header__item')[5];
+
+   
+    
+
+});
