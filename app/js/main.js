d383f9e0ecc57869009e0fc7f199f48750b59767
vladimir <rayevskiy.volodya@mail.ru>
2021-03-11 00:15:06 +0300

    designed a video section about us and our history

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,8 @@
 window.addEventListener('DOMContentLoaded', ()=> {
-'use strict';
+  'use strict';
 
 
-    const firstElement = document.querySelectorAll('.header__item')[0];
-    const lastElement = document.querySelectorAll('.header__item')[5];
-
-   
+ 
     
 
 });
