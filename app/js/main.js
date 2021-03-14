ca195dd115f4c476a170803d4dcf30c0f657e5b7
vladimir <rayevskiy.volodya@mail.ru>
2021-03-14 21:54:38 +0300

    adapted the site header

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,20 @@ window.addEventListener('DOMContentLoaded', ()=> {
   'use strict';
 
 
- 
-    
+  //mobile menu
 
+  const menuBtn = document.querySelector('.mobile-button'),
+        mobileNav = document.querySelector('.mobile__nav'),
+        buttonClose = document.querySelector('.mobile__close'),
+        superimposition = document.querySelector('.superimposition');
+
+        menuBtn.addEventListener('click', ()=> {
+          mobileNav.classList.add('mobile__nav--active');
+          document.body.overflow = 'hidden';
+          superimposition.classList.add('active');
+        });
+        buttonClose.addEventListener('click', ()=> {
+          mobileNav.classList.remove('mobile__nav--active');
+          superimposition.classList.remove('active');
+        });
 });
